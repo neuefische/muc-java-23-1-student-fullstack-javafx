31cdecc69237a86b309628275dc07aff95304558
Sergej Grilborzer <sergej@grilborzer.de>
2023-07-18 16:33:29 +0200

    Refactor field names in Login & CreateAccountController & use their data

--- frontend/src/main/java/de/neuefische/mucjava231javafxstudents/controller/CreateAccountViewController.java
+++ frontend/src/main/java/de/neuefische/mucjava231javafxstudents/controller/CreateAccountViewController.java
@@ -2,15 +2,28 @@ package de.neuefische.mucjava231javafxstudents.controller;
 
 import de.neuefische.mucjava231javafxstudents.service.SceneSwitchService;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class CreateAccountViewController {
 
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label errorMessageLabel;
+
     private final SceneSwitchService sceneSwitchService = SceneSwitchService.getInstance();
 
     public void createNewAccount(ActionEvent event) {
-
+        System.out.println(emailTextField.getText() + " " + userNameTextField.getText() + " " + passwordField.getText());
     }
 
     public void switchToLoginView(ActionEvent event) throws IOException {
--- frontend/src/main/java/de/neuefische/mucjava231javafxstudents/controller/LoginViewController.java
+++ frontend/src/main/java/de/neuefische/mucjava231javafxstudents/controller/LoginViewController.java
@@ -2,10 +2,21 @@ package de.neuefische.mucjava231javafxstudents.controller;
 
 import de.neuefische.mucjava231javafxstudents.service.SceneSwitchService;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class LoginViewController {
+
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label errorMessageLabel;
 
     private final SceneSwitchService sceneSwitchService = SceneSwitchService.getInstance();
 
@@ -14,5 +25,6 @@ public class LoginViewController {
     }
 
     public void login(ActionEvent event) {
+        System.out.println(userNameTextField.getText() + " " + passwordField.getText());
     }
 }
