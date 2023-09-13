421a22dc04aacfd6773e0ee847053f662a9c65d2
Sergej Grilborzer <sergej@grilborzer.de>
2023-09-13 16:28:06 +0200

    Add JaCoCo Maven Plugin

---
+++ backend/src/test/java/de/neuefische/studentdbbackend/StudentdbBackendApplicationTest.java
@@ -0,0 +1,15 @@
+package de.neuefische.studentdbbackend;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentdbBackendApplicationTest {
+
+    void test() {
+
+    }
+
+
+}
