963082a46a2d7aef264fae035f3cbcab0c87e267
Sergej Grilborzer <sergej@grilborzer.de>
2023-09-28 15:38:30 +0200

    Create test cases for PUT, DELETE & the rest

--- backend/src/test/java/de/neuefische/studentdbbackend/student/StudentControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/studentdbbackend/student/StudentControllerIntegrationTest.java
@@ -13,11 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WithMockUser(username = "user")
 @SpringBootTest
 @AutoConfigureMockMvc
 class StudentControllerIntegrationTest {
@@ -41,7 +41,6 @@ class StudentControllerIntegrationTest {
         );
     }
 
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
     @DirtiesContext
     @Test
     void getAllStudents_shouldReturnAllEntries_whenTwoEntriesAreSaved() throws Exception {
@@ -54,9 +53,12 @@ class StudentControllerIntegrationTest {
 
     @Test
     void searchStudent() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/search?firstName=" + student1.getFirstName() + "&lastName=" + student1.getLastName()))
+                .andExpect(jsonPath("matriculationNumber").isNotEmpty())
+                .andExpect(jsonPath("firstName").value(student1.getFirstName()))
+                .andExpect(jsonPath("lastName").value(student1.getLastName()));
     }
 
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
     @Test
     void getStudentByMatriculationNumber_shouldReturnRequestedStudent_whenMatchingMatriculatioNumberIsProvided() throws Exception {
         String studentsListAsString = mockMvc.perform(get(BASE_URL))
@@ -70,7 +72,6 @@ class StudentControllerIntegrationTest {
                 .andExpect(jsonPath("matriculationNumber").value(firstStudent.getMatriculationNumber()));
     }
 
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
     @DirtiesContext
     @Test
     void addStudent_shouldCreateNewStudentWithMatricleNumber_whenValidDataIsProvided() throws Exception {
@@ -85,7 +86,6 @@ class StudentControllerIntegrationTest {
                 );
     }
 
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
     @DirtiesContext
     @Test
     void addStudent_shouldReturn400_whenInvalidDataIsProvided() throws Exception {
@@ -115,12 +115,109 @@ class StudentControllerIntegrationTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @DirtiesContext
     @Test
-    void updateStudent() throws Exception {
+    void updateStudent_shouldUpdateStudentAccordingly_whenUserHasAdminRoleAndStudentExists() throws Exception {
+        String studentsListAsString = mockMvc.perform(get(BASE_URL))
+                .andReturn().getResponse().getContentAsString();
+
+        List<StudentResponseDto> studentsList = objectMapper.readValue(studentsListAsString, new TypeReference<>() {
+        });
+        StudentResponseDto originalStudent = studentsList.get(0);
+        NewStudentDto updatedFirstStudent = new NewStudentDto("Papa", originalStudent.getLastName(), originalStudent.getEmail(), originalStudent.getCourseOfStudies());
+
+        mockMvc.perform(put(BASE_URL + "/" + originalStudent.getMatriculationNumber())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(updatedFirstStudent)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("matriculationNumber").value(originalStudent.getMatriculationNumber()))
+                .andExpect(jsonPath("firstName").value(updatedFirstStudent.getFirstName()))
+                .andExpect(jsonPath("lastName").value(updatedFirstStudent.getLastName())
+                );
+    }
+
+    @DirtiesContext
+    @Test
+    void updateStudent_shoulReturn400_whenValuesInBodyArentValidAndMatriculationNumberIsInvalid() throws Exception {
+        String THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST = "THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST";
+        NewStudentDto studentThatDoesntExist = new NewStudentDto("?", "?", "?", "?");
+
+        mockMvc.perform(put(BASE_URL + "/" + THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST)
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(studentThatDoesntExist)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @DirtiesContext
+    @Test
+    void updateStudent_shoulReturn400_whenValuesInBodyArentValidAndMatriculationNumberIsValid() throws Exception {
+        String studentsListAsString = mockMvc.perform(get(BASE_URL))
+                .andReturn().getResponse().getContentAsString();
+
+        List<StudentResponseDto> studentsList = objectMapper.readValue(studentsListAsString, new TypeReference<>() {
+        });
+        StudentResponseDto originalStudent = studentsList.get(0);
+
+        NewStudentDto invalidRequestBody = new NewStudentDto("?", "?", "?", "?");
+
+        mockMvc.perform(put(BASE_URL + "/" + originalStudent.getMatriculationNumber())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(invalidRequestBody)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @DirtiesContext
+    @Test
+    void updateStudent_shoulReturn404_whenStudentDoesntExist() throws Exception {
+        String THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST = "THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST";
+        NewStudentDto studentThatDoesntExist = new NewStudentDto("Nobody", "Nobody", "Nobody@Nobody.de", "Nobody");
+
+        mockMvc.perform(put(BASE_URL + "/" + THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST)
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(studentThatDoesntExist)))
+                .andExpect(status().isNotFound());
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @DirtiesContext
+    @Test
+    void deleteStudent_shouldDeleteStudentByMatriculationNumber_whenStudentExistsAndUserHasAdminRole() throws Exception {
+        String studentsListAsString = mockMvc.perform(get(BASE_URL))
+                .andReturn().getResponse().getContentAsString();
+
+        List<StudentResponseDto> studentsList = objectMapper.readValue(studentsListAsString, new TypeReference<>() {
+        });
+        StudentResponseDto studentToDelete = studentsList.get(0);
+
+        mockMvc.perform(delete(BASE_URL + "/" + studentToDelete.getMatriculationNumber()))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get(BASE_URL + "/" + studentToDelete.getMatriculationNumber()))
+                .andExpect(status().isNotFound());
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @DirtiesContext
+    @Test
+    void deleteStudent_shouldReturn404_whenStudentDoesntExistAndUserHasAdminRole() throws Exception {
+        String THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST = "THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST";
+
+        mockMvc.perform(delete(BASE_URL + "/" + THIS_MATRICULATION_NUMBER_DOES_NOT_EXIST))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void deleteStudent() throws Exception {
+    void deleteStudent_shouldReturn403_whenUserHasNoAdminRole() throws Exception {
+        String studentsListAsString = mockMvc.perform(get(BASE_URL))
+                .andReturn().getResponse().getContentAsString();
+
+        List<StudentResponseDto> studentsList = objectMapper.readValue(studentsListAsString, new TypeReference<>() {
+        });
+        StudentResponseDto originalStudent = studentsList.get(0);
+
+        mockMvc.perform(delete(BASE_URL + "/" + originalStudent.getMatriculationNumber()))
+                .andExpect(status().isForbidden());
     }
 
     @Test
