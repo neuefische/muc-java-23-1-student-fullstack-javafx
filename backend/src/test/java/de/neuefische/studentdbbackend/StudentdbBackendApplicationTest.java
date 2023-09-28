3767c75b6706ba279a77720a2927b11f336fdc1e
Sergej Grilborzer <sergej@grilborzer.de>
2023-09-28 17:03:04 +0200

    Fix Sonar warning on "empty test case"

--- backend/src/test/java/de/neuefische/studentdbbackend/StudentdbBackendApplicationTest.java
+++ backend/src/test/java/de/neuefische/studentdbbackend/StudentdbBackendApplicationTest.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class StudentdbBackendApplicationTest {
 
+    // As kindly suggested by https://stackoverflow.com/a/64362419/11515036
     @Test
+    @SuppressWarnings("squid:S2699")
     public void applicationContextLoads() {
-
     }
 }
